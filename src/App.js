6faa493aef7eff710a32f03b0cb1ff24c78da82c
BlackWind001 <msanirudh1998@gmail.com>
2021-06-27 17:38:19 +0530

    Setup the basic app with css loaders.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+function App (props) {
+    return (
+        <div className='none'>
+            Hello World
+        </div>
+    )
+}
+
+export default hot(module)(App);
